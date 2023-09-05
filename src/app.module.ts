7e707359e452cb23a5f3dd2820f6a621d85ff6b2
Bedo <bedonassef71@gmail.com>
2023-09-05 17:03:03 +0300

    connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DatabaseModule } from "./database/database.module";
+import { ConfigModule } from "@nestjs/config";
+import { configSchemaValidation } from "./common/validation/config-schema.validation";
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ expandVariables: true, validationSchema: configSchemaValidation }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
